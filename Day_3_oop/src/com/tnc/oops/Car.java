76c4f0dade6fedaef3625f2f87a20c93a79ba5b3
Rsai <Rsai@DESKTOP-2IGNOKO>
2022-03-31 21:06:51 +0530

    First commit

---
+++ Day_3_oop/src/com/tnc/oops/Car.java
@@ -0,0 +1,17 @@
+package com.tnc.oops;
+
+public class Car 
+{
+	void display()
+	{
+		System.out.println("Welcome to c2tc program");
+	}
+
+	public static void main(String[] args) 
+	{
+		Car c =new Car();
+		c.display();
+
+	}
+
+}
