64e812e6fef90703ed50880709babf132cb260be
Rsai <Rsai@DESKTOP-2IGNOKO>
2022-03-31 21:05:14 +0530

    First commit

---
+++ Day_2_introduction _to_java/src/com/tns/introduction/Example1.java
@@ -0,0 +1,13 @@
+package com.tns.introduction;
+
+public class Example1 {
+
+	public static void main(String[] args)
+	{
+		int a=10;
+		System.out.println(a);
+	
+
+	}
+
+}
---
+++ Day_2_introduction _to_java/src/com/tns/introduction/Example3.java
@@ -0,0 +1,21 @@
+package com.tns.introduction;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		int a=5, b=10;
+		boolean b1=true;
+		boolean b2=false;
+		if(a>b)
+		{
+			System.out.println(b1);
+		}
+		else
+		{
+			System.out.println(b2);
+			
+		}
+
+	}
+
+}
