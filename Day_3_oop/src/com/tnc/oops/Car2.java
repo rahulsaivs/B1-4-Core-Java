9f984815265bd5b781029c8b62ab9246094c8fea
Rsai <Rsai@DESKTOP-2IGNOKO>
2022-04-01 09:49:51 +0530

    First commit

---
+++ Day_3_oop/src/com/tnc/oops/Car2.java
@@ -0,0 +1,26 @@
+package com.tnc.oops;
+
+public class Car2 
+{
+	String str;
+	Car2(String s)
+	{
+		str= s;
+	}
+	void print()
+	{
+		System.out.println("this is"+ str);
+	}
+
+	public static void main(String[] args)
+	{
+		Car2 a= new Car2(" AUDI");
+		Car2 a1=new Car2(" BMW");
+		Car2 a2=new Car2(" MARCIDES BENZ");
+		a.print();
+		a1.print();
+		a2.print();
+		
+	}
+
+}
