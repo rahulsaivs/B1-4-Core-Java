0d4c200c90e1601388e830580b2acb9a41b785b7
Rsai <Rsai@DESKTOP-2IGNOKO>
2022-04-05 15:06:37 +0530

    First commit

---
+++ Assigment1/src/com/tns/looping/Unicode.java
@@ -0,0 +1,17 @@
+package com.tns.looping;
+
+import java.util.Scanner;
+
+public class Unicode {
+
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		char c='\u0021';
+		System.out.println((int)c);
+		s.close();
+		
+
+	}
+
+}
