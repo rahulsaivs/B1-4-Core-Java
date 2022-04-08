d6083453495f32122f336eda9ed58cb8f2a5b096
Rsai <Rsai@DESKTOP-2IGNOKO>
2022-04-08 18:50:52 +0530

    First commit

---
+++ Day10_Array/src/com/tnc/array/Example2.java
@@ -0,0 +1,14 @@
+package com.tnc.array;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		int arr[]= {5,10,15,20};
+		for(int i=0;i<arr.length;i++)
+		{
+			 System.out.println(arr[i]);
+		}
+	     
+	}
+
+}
---
+++ Day10_Array/src/com/tnc/array/Example5.java
@@ -0,0 +1,10 @@
+package com.tnc.array;
+
+public class Example5 {
+
+	public static void main(String[] args) {
+		int arr[]= {5,10,15,20};
+		System.out.println(arr.getClass().getName());
+	}
+
+}
